e1837fd5cb8385329d8b534eec69f9b53cbc822e
unknown <pingbao.liu@kingstar.com>
2018-08-21 15:40:22 +0800

    'Readme.txt'

---
+++ FlaskModles/FlaskWeb/Readme.txt
@@ -0,0 +1,6 @@
+《Flask Web开发——基于Python的Web应用开发实践》
+1、创建数据库  python manage.py db upgrade
+2、更新数据库  python manage.py db upgrade
+3、回退数据库  python manage.py db downgrade
+
+4、启动服务 python manage.py runserver
--- FlaskModles/FlaskWeb/scripts/db_scripts/db_downgrade.py
+++ FlaskModles/FlaskWeb/scripts/db_scripts/db_downgrade.py
@@ -6,7 +6,6 @@
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO
 
-v = api.db_version(SQLALCHEMY_DATABASE_URI, SQ
-L)
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 api.downgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, v - 1)
 print('Current database version:' + str(api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)))
